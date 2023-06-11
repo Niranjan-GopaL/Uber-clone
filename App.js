93dcdd0ccf22dfb231487b696cc695c32018995d
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-11 22:58:33 +0530

    Notes and a working model yaygsgs

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Uber clone yay!!!</Text>
+      <Text>Uber clone yay!!!</Text>
+      <Text>Uber clone yay!!!</Text>
+      <Text>Uber clone yay!!!</Text>
+      <Text>Uber clone yay!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
