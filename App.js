6a18bf0bc0c03dc3ff89946dc096b24d51ea8aa4
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-12 06:51:22 +0530

    everything worksgsgs I RAN AND SOO THE LOGO OF UBER I ALMOST CRIEDD THIS IS SOOO COOL

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Dimensions } from 'react-native'
 import React from 'react'
 
+import HomeScreen from './src/screens/HomeScreen'
 import { colors } from './src/globals/styles'
+
+const SCREEN_WIDTH = Dimensions.get("window").width
+
+
 
 const App = () => {
   return (
     <View>
       <Text> Hello world !!!! </Text>
+      <HomeScreen />
     </View>
   )
 }
