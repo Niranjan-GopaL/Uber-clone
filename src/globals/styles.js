2a1b5d917429c33a178da2e58e8907edeb79da56
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-12 06:06:11 +0530

    styled buttons and titles

--- src/globals/styles.js
+++ src/globals/styles.js
@@ -33,6 +33,37 @@ export const colors = {
     darkBlue:"#2d328a",
     pagebackground:"white"
 
-    
 
+}
+
+export const parameters ={
+    statusBarHeight :getStatusBarHeight(),
+    headerHeight:70,
+
+    styledButton:{
+        backgroundColor:"#ff8c52",
+        alignContent:"center",
+        justifyContent:"center",
+        borderRadius:12,
+        borderWidth:1, 
+        borderColor:"#ff8c52",
+        height:50,
+        paddingHorizontal:20,
+        width:'100%'
+    },
+
+    buttonTitle:{
+        color:"white",
+        fontSize:20,  
+        fontWeight:"bold" ,
+        alignItems:"center",
+        justifyContent:"center"  ,
+        marginTop:-3 
+    }
+}
+
+export const title ={
+    color:"#ff8c52",
+    fontSize :20,
+    fontWeight:"bold"
 }
