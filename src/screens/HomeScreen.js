3c3d5ca2711b9df9f608e727f6cfd73d7b2b249a
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-12 07:06:22 +0530

    home text1 text2

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,27 +14,52 @@ const HomeScreen = () => {
 export default HomeScreen
 
 const styles = StyleSheet.create({
+
     container:{
         flex:1,
         backgroundColor:colors.white,
         paddingBottom:30,
         paddingTop:parameters.statusBarHeight
     },
+
     header:{
       backgroundColor:colors.blue,
       height:parameters.headerHeight,
       alignItems:"flex-start"
-     
     },
     
     image1:{
-     
       height:100,
       width:100,
-    
     },
     
-    image2:{height:60,width:60,
-            borderRadius:30,
-          },
+    image2:{
+        height:60,width:60,
+        borderRadius:30,
+    },
+
+    home:{
+        backgroundColor:colors.blue,
+        paddingLeft:20,
+    },
+       
+    text1:{
+        color:colors.white,
+        fontSize:21,
+        paddingBottom:20,
+        paddingTop:20
+    },
+       
+    text2:{
+        color:colors.white,
+        fontSize:16
+    },
+       
+    view1:{
+        flexDirection:"row",
+        flex:1,
+        paddingTop:30
+    },
+
+
 })
