53b4c763890d2596514d10fc41034467898784c1
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-08-10 15:10:29 +0530

    Identified what was creating error

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -16,7 +16,6 @@ const HomeScreen = () => {
 
     return (
 
-
         // Views are kinda like divs
         <View style={styles.container} >
             <View style={styles.header}>
@@ -119,9 +118,17 @@ const HomeScreen = () => {
                     </View>
                 </ScrollView>
 
+
+
+{/* ------------------------------- error is here ---------------------------------------------- */}
                 {/* We also need the status bar of the mobile to be blue ==> match our color palatte */}
                 {/* Now the status bar is awesome */}
-                <StatusBar style='' backgroundColor='#2058c0' translucent={{}}  />
+                {/* <StatusBar style='' backgroundColor='#2058c0' translucent={{}}  /> */}
+{/* ------------------------------- error is here ---------------------------------------------- */}
+
+
+
+
 
             </View>
         </View>
@@ -155,156 +162,178 @@ export default HomeScreen;
 
 // There are few stuff that you need to note
 const styles = StyleSheet.create({
-
-    container: {
-        flex: 1,
-        backgroundColor: colors.white,
-        paddingBottom: 30,
-
-        // the yarn add react-native-status-bar-height thing was for this
-        // WHAT is parameters here ?  => imported from global styles
-        paddingTop: parameters.statusBarHeight,
+    container:{
+        flex:1,
+        backgroundColor:colors.white,
+        paddingBottom:30,
+        paddingTop:parameters.statusBarHeight
+    },
+    header:{
+      backgroundColor:colors.blue,
+      height:parameters.headerHeight,
+      alignItems:"flex-start"
+     
     },
     
-    header: {
-        backgroundColor: colors.blue,
-        height: parameters.headerHeight,
-
-        // We are already paddingtoppping the container so no need to do it again, this was the fault
-        // paddingTop: parameters.statusBarHeight,
-
-        alignItems: "flex-start",
+    image1:{
+     
+      height:100,
+      width:100,
+    
     },
-
-    image1: { height: 100, width: 100 },
-
-    image2: {height: 60,width: 60,borderRadius: 30},
-
-    home: {backgroundColor: colors.blue,paddingLeft: 20,},
-
-    text1: {
-        color: colors.white,
-        fontSize: 21,
-        paddingBottom: 20,
-        paddingTop: 20,
+    
+    image2:{height:60,width:60,
+            borderRadius:30,
+          },
+    
+    home:{
+     backgroundColor:colors.blue,
+     paddingLeft:20,
+     
     },
-
-    text2: {
-        color: colors.white,
-        fontSize: 16,
+    
+    text1:{
+     color:colors.white,
+     fontSize:21,
+     paddingBottom:20,
+     paddingTop:20
     },
-
-    view1: {
-        flexDirection: "row",
-        flex: 1,
-        paddingTop: 30,
+    
+    text2:{
+     color:colors.white,
+     fontSize:16
     },
-
-    button1: {
-        height: 40,
-        width: 150,
-        backgroundColor: colors.black,
-        borderRadius: 20,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 20,
+    
+    view1:{
+     flexDirection:"row",
+     flex:1,
+     paddingTop:30
     },
-
-    button1Text: {
-        color: colors.white,
-        fontSize: 17,
-        marginTop: -2,
+    
+    button1:{
+      height:40,
+      width:150,
+      backgroundColor:colors.black,
+      borderRadius:20,
+      alignItems:"center",
+      justifyContent:"center",
+      marginTop:20
     },
-
-    card: {
-        alignItems: "center",
-        margin: SCREEN_WIDTH / 22,
+    
+    button1Text:{
+     color:colors.white,
+     fontSize:17,
+     marginTop:-2
+    
     },
-
-    view2: { marginBottom: 5, borderRadius: 15, backgroundColor: colors.grey6 },
-
-    title: {
-        color: colors.black,
-        fontSize: 16,
+    card:{
+     alignItems:"center",
+     margin:SCREEN_WIDTH/22
+    
     },
-
-    view3: {
-        flexDirection: "row",
-        marginTop: 5,
-        height: 50,
-        backgroundColor: colors.grey6,
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginHorizontal: 15,
+    
+    view2:{marginBottom:5,
+          borderRadius:15,
+          backgroundColor:colors.grey6
+        },
+    
+        title:{
+          color:colors.black,
+          fontSize:16
+        },
+    view3:{flexDirection:"row",
+             marginTop :5,
+             height:50,
+             backgroundColor:colors.grey6,
+             alignItems:"center",
+             justifyContent:"space-between",
+            marginHorizontal:15
+            
+             },
+    text3:{marginLeft:15,
+            fontSize:20,
+            color:colors.black
+      },
+    
+    view4:{ flexDirection:"row",
+            alignItems:"center",
+            marginRight:15,
+            backgroundColor:"white",
+            paddingHorizontal:10,
+            paddingVertical:2,
+            borderRadius:20
+            },
+    
+    view5:{ flexDirection:"row",
+    alignItems:"center",
+    backgroundColor:"white",
+    paddingVertical:25,
+    justifyContent:"space-between",
+    marginHorizontal:15,
+    borderBottomColor:colors.grey4,
+    borderBottomWidth:1,
+    flex:1
     },
-    text3: { marginLeft: 15, fontSize: 20, color: colors.black },
-
-    view4: {
-        flexDirection: "row",
-        alignItems: "center",
-        marginRight: 15,
-        backgroundColor: "white",
-        paddingHorizontal: 10,
-        paddingVertical: 2,
-        borderRadius: 20,
+    
+    view6:{
+    
+    
+    alignItems:"center",
+    flex:5,
+    flexDirection:"row"
     },
-
-    view5: {
-        flexDirection: "row",
-        alignItems: "center",
-        backgroundColor: "white",
-        paddingVertical: 25,
-        justifyContent: "space-between",
-        marginHorizontal: 15,
-        borderBottomColor: colors.grey4,
-        borderBottomWidth: 1,
-        flex: 1,
+    view7:{
+    backgroundColor:colors.grey6,
+    height:40,
+    width:40,
+    borderRadius:20,
+    alignItems:"center",
+    justifyContent:"center",
+    marginRight:20
+    
     },
-
-    view6: {
-        alignItems: "center",
-        flex: 5,
-        flexDirection: "row",
+    
+    map:{
+       
+    height: 150,
+     marginVertical: 0,
+     width:SCREEN_WIDTH*0.92
     },
-    view7: {
-        backgroundColor: colors.grey6,
-        height: 40,
-        width: 40,
-        borderRadius: 20,
-        alignItems: "center",
-        justifyContent: "center",
-        marginRight: 20,
-    },
+    
+    text4:{ fontSize:20,
+          color:colors.black,
+          marginLeft:20,
+          marginBottom:20
+        },
+    
+    icon1:  {marginLeft:10,
+           marginTop:5
+          },
 
-    map: {
-        height: 150,
-        marginVertical: 0,
-        width: SCREEN_WIDTH * 0.92,
-    },
-
-    text4: {
-        fontSize: 20,
-        color: colors.black,
-        marginLeft: 20,
-        marginBottom: 20,
-    },
-
-    icon1: { marginLeft: 10, marginTop: 5 },
-
-    view8: { flex: 4, marginTop: -25 },
+    view8: {flex:4,
+          marginTop:-25
+        } ,
     carsAround: {
-        width: 28,
-        height: 14,
-    },
-
+    width: 28,
+    height: 14,
+    
+    }, 
+    
     location: {
-        width: 16,
-        height: 16,
-        borderRadius: 8,
-        backgroundColor: colors.blue,
-        alignItems: "center",
-        justifyContent: "center",
-    },
+      width: 16,
+      height: 16,
+      borderRadius:8,
+      backgroundColor:colors.blue,
+      alignItems:"center",
+      justifyContent:"center"
+      
+      }, 
+      
+    view9:{width:4,
+    height:4,
+    borderRadius:2,
+    backgroundColor:"white"
+    }
 
-    view9: { width: 4, height: 4, borderRadius: 2, backgroundColor: "white" },
-});
+
+})
